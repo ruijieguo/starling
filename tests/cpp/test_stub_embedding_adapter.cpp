2ab00e664c6dfc45f920100b682f1b38ef21c1e3
jared_guo <ruijieguo@gmail.com>
2026-07-02 20:40:36 +0800

    feat(embedding): add EmbeddingAdapter::embed_batch (default loops embed)

--- tests/cpp/test_stub_embedding_adapter.cpp
+++ tests/cpp/test_stub_embedding_adapter.cpp
@@ -1,6 +1,8 @@
 // tests/cpp/test_stub_embedding_adapter.cpp
 #include "starling/embedding/embedding_adapter.hpp"
 #include <gtest/gtest.h>
+#include <string>
+#include <vector>
 using namespace starling::embedding;
 
 TEST(StubEmbeddingAdapter, DeterministicSameTextSameVector) {
@@ -16,4 +18,21 @@ TEST(StubEmbeddingAdapter, FailNextThrowsOnce) {
     a.fail_next("boom");
     EXPECT_THROW(a.embed("boom"), EmbeddingError);
     EXPECT_NO_THROW(a.embed("boom"));
+}
+TEST(StubEmbeddingAdapter, EmbedBatchParityWithSingles) {
+    StubEmbeddingAdapter a(8);
+    std::vector<std::string> texts{"alpha", "beta", "gamma"};
+    auto batch = a.embed_batch(texts);
+    ASSERT_EQ(batch.size(), texts.size());
+    for (size_t pos = 0; pos < texts.size(); ++pos) {
+        auto single = a.embed(texts[pos]);
+        EXPECT_EQ(batch[pos].vector, single.vector);
+        EXPECT_EQ(batch[pos].dim, single.dim);
+    }
+}
+TEST(StubEmbeddingAdapter, EmbedBatchPropagatesFailure) {
+    StubEmbeddingAdapter a(8);
+    a.fail_next("beta");  // one text in the batch throws
+    std::vector<std::string> texts{"alpha", "beta", "gamma"};
+    EXPECT_THROW(a.embed_batch(texts), EmbeddingError);
 }
